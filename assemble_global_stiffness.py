598f2d3b7dde253b48330ef09512742a7aecd7c8
Apoorv Garg <apoorvgarg14@gmail.com>
2017-07-01 15:25:09 -0400

    Global Stiffness Assembly
    - For Isotropic Material
    - Same DOF on all Nodes

---
+++ assemble_global_stiffness.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  1 10:30:41 2017
+
+@author: Apoorv
+Purpose - Return assembled global stiffness matrix 
+        - The material is assumed to be isotropic
+        - All the nodes are assumed to have same degree of freedom
+        - Elem Connectivity Follows GID mesh rules
+"""
+import numpy as np
+
+def assemble_global_stiffness(self,elm_stiff,elem_connectivity,data):
+    num_of_elem = getNumOfElem(elem_connectivity)
+    num_of_nodes_elem = getNumOfNodes(elem_connectivity)
+    dof_node = getDof(data)
+
+    for elem_num in num_of_elem:
+        for a in num_of_nodes_elem:
+            for i in dof_node:
+                for b in num_of_nodes_elem:
+                    for j in dof_node:
+                        row = dof_node*(elem_connectivity(elem_num,a+1))+i    # Not sure how elem_connectivity table will look like
+                        col = dof_node*(elem_connectivity(elem_num,b+1))+j
+                        gobal_stiffness(row,col) = global_stiffness(row,col) + elem_stiffness((dof_node*a)+i,(dof_node*b)+j)  # Cross-check the valus of elem_stiffness corresponding to global position
+    
+    return global_stiffness
+
+def getNumOfElem(self,connectivity):
+    return len(connectivity)
+
+def getNumOfNodes(self, connectivity):
+    return (len(connectivuty[0])-1)
+
+def getDof(data):
+    return 2    #Finalize on how to decide degree of freedom
+
+                
