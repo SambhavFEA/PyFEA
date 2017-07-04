cfb41e264110e177457c5b6c03b35fef6f7d052f
Apoorv Garg <apoorvgarg14@gmail.com>
2017-07-04 17:18:16 -0400

    Read Input Debug
    Major changes in the Read function done with decision on how to store data. Next task is to debug and integrate read function in model class

--- Read_input.py
+++ Read_input.py
@@ -1,51 +1,46 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  3 20:08:23 2017
-
-@author: Apoorv
-"""
 import numpy as np
+#Initializing
 
-def read_fea_input(self, filename):
-    
-    #Initializing
-    NodeX = []
-    NodeY = []
-    Elem = []
-    Material = []
-    bc = []
-    ff =[]
+
+def read_input_2D(self,filename):
     
     f = open(filename,'r')
     lines = f.readlines()
     
     #Node Coordinate Matrix
-    num_node_coord = int(lines[2][0])
-    
+    num_node_coord = int(lines[2])
+    Node = np.zeros([num_node_coord,2])    
     for i in range(num_node_coord):
-        NodeX[i] = float(lines[2+i][2:5])
-        NodeY[i] = float(lines[2+i][6:9])
-    
+        Node[i] = list(map(float,lines[2+i+1].split(" ")))
+   
     #Connectivity Matrix
     num_elem = int(lines[7+num_node_coord][0]) 
+    #Dynamic nnpe (nnpe stays const)
+    nnpe = np.shape((list(map(int,lines[8+num_node_coord].split(" "))))[0])
     
+    Elem = np.zeros([num_elem,nnpe]) 
     for j in range(num_elem):
-        Elem[j] = list(map(int,lines[7+num_node_coord+j].split()))
+        Elem[j] = list(map(int,lines[7+num_node_coord+j+1].split(" ")))
         
     #Material Properties
-    num_of_mat_prop = int(lines[11+num_node_coord+num_elem])
+    num_of_mat_prop = int(lines[11+num_node_coord+num_elem])        #Hash Mapping can be used for different type of material properties. Dummy!
+    Material = np.zeros([num_of_mat_prop,1])
     for k in range(num_of_mat_prop):
-        Material[k] = int(lines[11+num_node_coord+num_elem+k])
+        Material[k] = int(lines[11+num_node_coord+num_elem+k+1])
 
     #Boundary Constraint
     num_of_bc = int(lines[15+num_node_coord+num_elem+num_of_mat_prop])
+    bc = np.zeros([num_of_bc,3])
     for l in range(num_of_bc):
-        bc[l] = list(map(int,lines[15+num_node_coord+num_elem+num_of_mat_prop+l]))
+        bc[l] = list(map(float,lines[15+num_node_coord+num_elem+num_of_mat_prop+l+1].split(" ")))
         
     #Force Constraint
     num_of_fc = int(lines[19+num_node_coord+num_elem+num_of_mat_prop+num_of_bc])
+    ff = np.zeros([num_of_fc,3])
     for m in range(num_of_fc):
-        ff[m] = list(map(int,lines[19+num_node_coord+num_elem+num_of_mat_prop+num_of_bc+m]))
-        
+        ff[m] = list(map(float,lines[19+num_node_coord+num_elem+num_of_mat_prop+num_of_bc+m+1].split(" ")))
     
+    pass
     
+read_input_2D("2DMeshTest.sam")
