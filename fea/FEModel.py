c8184c75cb009daf82881a682f68d67f98bdf6d7
amishgadigi <amishgadigi@gmail.com>
2017-07-06 18:43:57 -0400

    Added GUI

--- fea/FEModel.py
+++ fea/FEModel.py
@@ -1,24 +1,23 @@
-
 import numpy as np
 
-class Model:
 
-    ele = np.array([[],[4]])                #ele[element no, node no]   ---- ele[n,4] size
-    nodes = np.array([[],[2]])              #nodes[node no, coordinates]  ----- nodes[n,2] size
+class FEModel:
+    ele = np.array([[], [4]])  # ele[element no, node no]   ---- ele[n,4] size
+    nodes = np.array([[], [2]])  # nodes[node no, coordinates]  ----- nodes[n,2] size
     material = np.array([])
-    forces = np.array([],[3])               #forces[forceno,[node number, forcex, forcey]
-    boundary_constraints = np.array([], [3])          #constraints[constraint no,[node number, dof,val]]
+    forces = np.array([], [])  # forces[forceno,[node number, forcex, forcey]
+    boundary_constraints = np.array([], [])  # constraints[constraint no,[node number, dof,val]]
 
-    kStif = np.zeros([],[])
+    kStif = np.zeros([], [])
     uDisp = np.zeros([])
     fForce = np.zeros([])
 
     ndof = 2
     nnpe = 4
 
-    def _init_(self,elements,nodes,material,forces,fixtures):
-        #input must be elements, nodes and materials
-        ele=elements
+    def __init__(self, elements, nodes, material, forces, fixtures):
+        # input must be elements, nodes and materials
+        ele = elements
         self.nodes = nodes
         self.material = material
         self.forces = forces
@@ -26,15 +25,15 @@ class Model:
 
         nodesSize = np.shape(nodes)
 
-        kStif = np.zeros((nodesSize[0]*self.ndof,nodesSize[0]*self.ndof))
-        uDisp = np.zeros(nodesSize[0]*self.ndof)
-        fForce = np.zeros(nodesSize[0]*self.ndof)
+        kStif = np.zeros((nodesSize[0] * self.ndof, nodesSize[0] * self.ndof))
+        uDisp = np.zeros(nodesSize[0] * self.ndof)
+        fForce = np.zeros(nodesSize[0] * self.ndof)
         return
 
-    def _init_(self,filename):
-        self.read_input_2D(self,filename)
+    def __init__(self, filename):
+        self.read_input_2D(filename)
 
-    def read_input_2D(self,filename):
+    def read_input_2D(self, filename):
 
         f = open(filename, 'r')
         lines = f.readlines()
@@ -55,7 +54,8 @@ class Model:
             self.ele[j] = list(map(int, lines[7 + num_node_coord + j + 1].split(" ")))
 
         # Material Properties
-        num_of_mat_prop = int(lines[11 + num_node_coord + num_elem])  # Hash Mapping can be used for different type of material properties. Dummy!
+        num_of_mat_prop = int(lines[
+                                  11 + num_node_coord + num_elem])  # Hash Mapping can be used for different type of material properties. Dummy!
         self.material = np.zeros([num_of_mat_prop, 1])
         for k in range(num_of_mat_prop):
             self.material[k] = float(lines[11 + num_node_coord + num_elem + k + 1])
@@ -64,19 +64,20 @@ class Model:
         num_of_bc = int(lines[15 + num_node_coord + num_elem + num_of_mat_prop])
         self.boundary_constraints = np.zeros([num_of_bc, 3])
         for l in range(num_of_bc):
-            self.boundary_constraints[l] = list(map(float, lines[15 + num_node_coord + num_elem + num_of_mat_prop + l + 1].split(" ")))
+            self.boundary_constraints[l] = list(
+                map(float, lines[15 + num_node_coord + num_elem + num_of_mat_prop + l + 1].split(" ")))
 
         # Force Constraint
         num_of_fc = int(lines[19 + num_node_coord + num_elem + num_of_mat_prop + num_of_bc])
         self.forces = np.zeros([num_of_fc, 3])
         for m in range(num_of_fc):
-            self.forces[m] = list(map(float, lines[19 + num_node_coord + num_elem + num_of_mat_prop + num_of_bc + m + 1].split(" ")))
+            self.forces[m] = list(
+                map(float, lines[19 + num_node_coord + num_elem + num_of_mat_prop + num_of_bc + m + 1].split(" ")))
 
 
 def readInput():
- return
+    return
 
 
 def main():
-
- return
+    return
