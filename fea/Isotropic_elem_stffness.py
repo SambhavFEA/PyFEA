3da52b922cb59c38bd019716aa6267eeb5b4ec85
Apoorv Garg <apoorvgarg14@gmail.com>
2017-07-09 17:06:28 -0400

    Test_Apoorv_Brancg

--- fea/Isotropic_elem_stffness.py
+++ fea/Isotropic_elem_stffness.py
@@ -5,7 +5,7 @@ def elem_stiff(model = FEModel.FEModel):
     E = model.material[0]
     nu = model.material[1]
 
-    model.k = (E/(1+nu)*(1-2*nu))* \
+    model.k = (E/(1+nu)*(1-3*nu))* \
         [[1-nu, nu, nu, 0., 0., 0.],
          [nu, 1-nu, nu, 0., 0., 0.],
          [nu, nu, 1-nu, 0., 0., 0.],
