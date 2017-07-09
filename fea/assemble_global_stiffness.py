c6950834ee17b5effd6e1306b90eb4bba228d0e0
amishgadigi <amishgadigi@gmail.com>
2017-07-09 15:13:45 -0400

    trying to commit
    Signed-off-by: amishgadigi <amishgadigi@gmail.com>

--- fea/assemble_global_stiffness.py
+++ fea/assemble_global_stiffness.py
@@ -9,34 +9,47 @@ Purpose - Return assembled global stiffness matrix
         - Elem Connectivity Follows GID mesh rules
 """
 import numpy as np
+import FEModel
+
+def assemble_global_stiffness(model = FEModel.FEModel):
+    num_of_elem = getNumOfElem(model)
+    num_of_nodes_elem = getNumOfNodes(model)
+    dof_node = getDof(model)
+    model.kStif = elem_stiff(model)
+    model.global_stiffness = np.zeros([],[])
+
+    for elem_num in range(num_of_elem):
+        for a in range(num_of_nodes_elem):
+            for i in range(dof_node):
+                for b in range(num_of_nodes_elem):
+                    for j in range(dof_node):
+                        row = dof_node*(int(model.ele[elem_num, a + 1])) + i    # Not sure how elem_connectivity table will look like
+                        col = dof_node*(int(model.ele[elem_num, b + 1])) + j
+                        model.global_stiffness[row][col] = model.global_stiffness[row][col] + model.kStif[(dof_node * a) + i, (dof_node * b) + j]  # Cross-check the valus of elem_stiffness corresponding to global position
 
 
-def assemble_global_stiffness(self,elm_stiff,elem_connectivity,data):
-    num_of_elem = getNumOfElem(elem_connectivity)
-    num_of_nodes_elem = getNumOfNodes(elem_connectivity)
-    dof_node = getDof(data)
-    global_stiffness = []
+def getNumOfElem(model = FEModel.FEModel):
+    return len(model.ele)
 
-    for elem_num in num_of_elem:
-        for a in num_of_nodes_elem:
-            for i in dof_node:
-                for b in num_of_nodes_elem:
-                    for j in dof_node:
-                        row = dof_node*(elem_connectivity(elem_num,a+1))+i    # Not sure how elem_connectivity table will look like
-                        col = dof_node*(elem_connectivity(elem_num,b+1))+j
-                        global_stiffness[row,col] = global_stiffness[row,col] + elm_stiff[(dof_node*a)+i,(dof_node*b)+j]  # Cross-check the valus of elem_stiffness corresponding to global position
-    
-    return global_stiffness
-
-def getNumOfElem(self,connectivity):
-    return len(connectivity)
-
-def getNumOfNodes(self, connectivity):
-    return (len(connectivity[0])-1)
+def getNumOfNodes(model = FEModel.FEModel):
+    return (len(model.nodes))
 
 def getDof(data):
     return 2    #Finalize on how to decide degree of freedom
 
+
+def elem_stiff(model=FEModel.FEModel):
+    E = model.material[0][0]
+    nu = model.material[1][0]
+
+    k = np.multiply((E/((1+nu)*(1-2*nu))),
+         [[1-nu, nu, nu, 0., 0., 0.],
+         [nu, 1-nu, nu, 0., 0., 0.],
+         [nu, nu, 1-nu, 0., 0., 0.],
+         [0., 0., 0., 1-(2*nu), 0., 0.],
+         [0., 0., 0., 0., 1-(2*nu), 0.],
+         [0., 0., 0., 0., 0., 1-(2*nu)]])
+    return k
 # Changes
 def apoorvname():
     return "Apoorv Garg"
