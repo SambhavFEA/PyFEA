54ebedf5f1517cb42a9e37f72290ca019f9dfacc
HP xw8400 <apoorvgarg14@gmail.com>
2017-08-11 15:21:45 -0400

    SolveFEModel producing result. However, Elastic limit of the results need to be verified.

--- fea/solveFEModel.py
+++ fea/solveFEModel.py
@@ -2,6 +2,5 @@ import numpy as np
 import FEModel
 
 def solveFEModel(model = FEModel.FEModel):
-    size_disp = len(model.fForce)
-    model.disp_result = np.array([size_disp])
-    model.disp_result = np.divide(model.fForce,model.global_stiffness)
+
+    model.disp_result = np.matmul(model.fForce,np.linalg.inv(model.global_stiffness))
