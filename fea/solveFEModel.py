e8392f7eab40e9c2913202ec3cb72b7c3626f039
HP xw8400 <apoorvgarg14@gmail.com>
2017-09-05 17:04:09 -0400

    resultantNodes method Added

--- fea/solveFEModel.py
+++ fea/solveFEModel.py
@@ -4,3 +4,12 @@ import FEModel
 def solveFEModel(model = FEModel.FEModel):
 
     model.disp_result = np.matmul(model.fForce,np.linalg.inv(model.global_stiffness))
+
+def resultantNodes (model = FEModel.FEModel):
+
+    tempArray = model.disp_result.reshape(6,2)
+    finalArray =  np.zeros(len(tempArray))
+    for i in model.nodes:
+        finalArray[i] = model.nodes[i] + tempArray[i]
+
+    return finalArray
