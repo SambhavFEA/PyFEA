bff4c888a341de87e003c44fd341625dc5e010f2
Apoorv Garg <apoorvgarg14@gmail.com>
2017-09-16 11:13:47 -0400

    fileI_O class created and debugged. fileI_O class working.
    fileI_o node and nnpe hardcoded - discuss amish.
    fileI_o itself feels like FEModel- discuss amish

--- fea/Run Script.py
+++ fea/Run Script.py
@@ -1,17 +1,21 @@
 import FEModel
 import numpy as np
+import fileI_O
 
 from assemble_global_stiffness import assemble_global_stiffness
 from apply_constraints import apply_constraints
 from solveFEModel import solveFEModel
-from saveFEModel import saveFEModel
+
 if __name__ == '__main__':
-    Mod = FEModel.FEModel('2DMeshTest.sam')
-    assemble_global_stiffness(Mod)
-    apply_constraints(Mod)
-    solveFEModel(Mod)
+    #Mod = FEModel.FEModel('2DMeshTest.sam')
+    #assemble_global_stiffness(Mod)
+    #apply_constraints(Mod)
+    #solveFEModel(Mod)
     #filename = 'test'
     #saveFEModel(filename,Mod)
-    print Mod.uDisp
+    fileI = fileI_O.fileI_O('2DMeshTest.sam')
+    fileI.saveFEModel('fileI_Otest')
+
+    #print Mod.uDisp
     pass
 
